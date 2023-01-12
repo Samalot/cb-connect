3bf4fd17b43f3fc3c3508222a49b63fa04a4d2c7
Sam Humby <samalotmedia@gmail.com>
2023-01-12 16:49:32 +0000

    added tests for getStats

--- src/loadSkills.ts
+++ src/loadSkills.ts
@@ -5,7 +5,7 @@ const metadataAddress = "0xec3e38e536ad4fa55a378b14b257976148b618ac";
 
 export const loadSkills = async (
   provider: any,
-  brokerID: string,
+  brokerID: string | number,
   contractAddress = metadataAddress
 ) => {
   const cyberBrokersMetadata = new ethers.Contract(contractAddress, metadataABI, provider);
--- tests/loadSkills.test.ts
+++ tests/loadSkills.test.ts
@@ -1,8 +1,42 @@
+// Define function mocks before imports
+const ethersContractMock = jest.fn(() => mockContract);
+const getStatsMock = jest.fn(() => Promise.resolve(mockStats));
 
 import { loadSkills } from '../src/loadSkills'; 
 
+const mockStats = {
+  mind: 10,
+  body: 10,
+  soul: 10
+}
+
+const mockContract = { getStats: getStatsMock };
+jest.mock('ethers', () => ({
+  ethers: { Contract: ethersContractMock }
+}));
+
 describe('testing index file', () => {
-  test('empty string should result in zero', () => {
-    expect(0).toBe(0);
+  beforeAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should return brokers stats', async () => {
+    const result = await loadSkills({}, "100");
+
+    expect(ethersContractMock).toBeCalled();
+    expect(getStatsMock).toBeCalled();
+    expect(result).toBe(mockStats);
+  });
+
+  test('should return error if ethers contract fails', async () => {
+    ethersContractMock.mockImplementationOnce(() => { throw new Error() });
+    await expect(loadSkills({}, "100"))
+      .rejects.toThrowError();
+  });
+
+  test('should return error if getStats fails', async () => {
+    getStatsMock.mockImplementationOnce(() => Promise.reject("getStats error"));
+    await expect(loadSkills({}, "100"))
+      .rejects.toEqual("getStats error");
   });
 });
